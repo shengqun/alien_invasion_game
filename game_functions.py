1ad80f2932588f75062821377440d59c908788ed
wuxiangjuezong <wuxiangjuezong@126.com>
2018-07-23 21:05:26 +0800

    修复BUG

--- game_functions.py
+++ game_functions.py
@@ -9,6 +9,7 @@ def check_keydown_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
         
+        
 def check_keyup_events(event, ship):
     '''响应松开'''
     if event.key == pygame.K_RIGHT:
@@ -16,6 +17,7 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
+        
 def check_events(ship):
     '''响应按键和鼠标事件'''
     for event in pygame.event.get():
@@ -25,22 +27,7 @@ def check_events(ship):
             check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
-        # elif event.type == pygame.KEYDOWN:
-            # if event.key == pygame.K_RIGHT:
-                # 向右移动标志置位
-                # ship.moving_right = True
-            # elif event.key == pygame.K_LEFT:
-                # 向左移动标志置位
-                # ship.moving_left = True
-                
-        # elif event.type == pygame.KEYUP:
-            # if event.key == pygame.K_RIGHT:
-                # 向右移动标志复位
-                # ship.moving_right = False
-            # elif event.key == pygame.K_LEFT:
-                # 向左移动标志复位
-                # ship.moving_left = False
-
+        
                 
 def update_screen(ai_settings, screen, ship):
     '''更新屏幕上的图像，并切换到新屏幕'''
